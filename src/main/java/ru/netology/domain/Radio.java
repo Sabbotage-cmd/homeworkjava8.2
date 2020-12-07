6f2f97105c2c13e88e6918d0e6f6c7bfff412e7d
Vladimir Mednis <mednisv@yandex.ru>
2020-12-07 13:46:04 +0300

    add lombok + rewrite data-class

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,84 +1,70 @@
 package ru.netology.domain;
 
-public class Radio{
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Radio {
     private int station;
     private int volume;
     private int count;
 
-    public Radio(int count){
-        this.count = count;
-    }
-
-    public int getStation() {
-        return station;
-    }
-
     public void setStation(int station) {
 
-        if (station < 0){
+        if (station < 0) {
             return;
         }
-
-        if (station > this.count){
+        if (station > this.count) {
             this.station = this.count;
             return;
         }
         this.station = station;
     }
 
-    public int getVolume() {
-        return volume;
-    }
-
     public void setVolume(int volume) {
-
-        if (volume > 100){
+        if (volume > 100) {
             this.volume = 100;
             return;
         }
-
-        if (volume < 0){
+        if (volume < 0) {
             return;
         }
         this.volume = volume;
     }
 
-    public void decreaseVolume(){
-        if (this.getVolume() == 0){
-            return;
-        }
-        this.volume--;
-    }
-
-    public void increaseVolume(){
+    public void increaseVolume() {
         if (this.getVolume() == 100) {
             return;
         }
         this.volume++;
     }
 
-    public void nextStation(){
-        if (this.getStation() == this.count){
+    public void decreaseVolume() {
+        if (this.getVolume() == 0) {
+            return;
+        }
+        this.volume--;
+    }
+
+    public void nextStation() {
+        if (this.getStation() == this.count) {
             this.setStation(0);
             return;
         }
         this.station++;
     }
 
-    public void previousStation(){
-        if (this.getStation() == 0){
+    public void previousStation() {
+        if (this.getStation() == 0) {
             this.setStation(this.count);
             return;
         }
         this.station--;
     }
 
-    public int getCount() {
-        return count;
-    }
 
-    public void setCount(int count) {
-        this.count = count;
-    }
 }
